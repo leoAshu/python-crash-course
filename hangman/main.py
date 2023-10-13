099dd559eba68f832364ac8ff9c486f2d6a0ba59
leoAshu <ashutosh.ojha2009@gmail.com>
2023-10-13 16:11:16 +0530

    hangman: complete game

--- hangman/main.py
+++ hangman/main.py
@@ -1,71 +1,23 @@
+import os
 import random
+from hangman_words import word_list
+from hangman_art import logo, stages
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
+print(logo)
 lives = 6
-word_list = ['aardvark', 'baboon', 'camel']
 
 chosen_word = random.choice(word_list)
 display = ['_'] * len(chosen_word)
 
+print(f'Pssst, the solution is {chosen_word}.')
+
 end_of_game = False
 while not end_of_game:
     guess = input('Guess a letter: ').lower()
+    os.system('cls')
+
+    if guess in display:
+        print(f'You\'ve already guessed {guess}.')
 
     for idx in range(len(chosen_word)):
         if chosen_word[idx] == guess:
@@ -74,6 +26,7 @@ while not end_of_game:
     print(' '.join(display))
     
     if guess not in chosen_word:
+        print(f'You guessed {guess}, that\'s not in the word. You lose a life.')
         lives -=1
         if lives == 0:
             end_of_game = True
